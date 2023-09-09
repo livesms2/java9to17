35ecad02c3000d3d8c8d1b337e5fbde915380880
shamim <live_sms2@yahoo.com>
2023-09-09 05:30:04 +0500

    java13

---
+++ index.js
@@ -0,0 +1,190 @@
+// ? Q No. 1 Write a program that take two numbers & add them in a new variable. Show the result in your browser
+
+// var numOne = 3;
+// var numTwo = 5;
+// var sum = numOne+numTwo;
+// document.write("Sum of two number is = " + sum);
+
+// ? Q Node. 2 (a) Repeat task1 for subtraction, multiplication, division & modulus
+
+// var numOne = 3;
+// var numTwo = 5;
+// var subtraction = numOne-numTwo;
+// document.write("subtraction of two number is = " - subtraction);
+
+// ? Q Node. 2 (b) 
+// var numOne = 3;
+// var numTwo = 5;
+// var multiplication = numOne*numTwo;
+// document.write("multiplication of two number is = " + multiplication);
+
+// ? Q Node. 2 (c) 
+// var numOne = 3;
+// var numTwo = 5;
+// var division = numOne/numTwo;
+// document.write("Division of two number is = " + division);
+
+/*// ? Q No. 3 Do the following using JS Mathematic Expressions
+// ? a. Declare a variable.
+
+var myVariable;
+
+// ? b. Show the value of variable in your browser like.
+
+document.write("Value after variable declaration is:" + myVariable + "<br>");
+
+// ? c. Initialize the variable with some number.
+
+myVariable = 5;
+
+// ? d. Show the value of variable in your browser like.
+
+document.write("Initial value:" + myVariable + "<br>");
+
+// ? e. Increment the variable.
+
+myVariable ++;
+
+// ? f. Show the value of variable in your browser like. 
+
+document.write("Value after increment is:" + myVariable + "<br>");
+
+// ? g. Add 7 to the variable.
+
+myVariable += 7;
+
+// ? h. Show the value of variable in your browser like.
+
+document.write("Value after addition is:" + myVariable + "<br>");
+
+// ? i. Decrement the variable.
+
+myVariable --;
+
+// ? j. Show the value of variable in your browser like.
+
+document.write("Value after decrement is:" + myVariable + "<br>")
+
+// ? k. Show the remainder after dividing the variable’s value by 3.
+
+var remainder = myVariable % 3;
+
+// ? l. Output : “The remainder is : 0”.
+
+document.write("The remainder is :" + remainder);
+
+*/
+
+/*// ? Q No. 4 Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets.
+
+var num1 = 600;
+var num2 = 5;
+var multiplication = num1*num2;
+document.write("The cost of buying 5 tickets is: " + multiplication);
+
+*/
+// ? Q No. 5 Write a script to display multiplication table of any number in your browser.
+
+// const number = parseInt(prompt("Enter an integer: "));
+// for(let i = 1; i <= 10; i++) {
+//     const result = i * number;
+//     console.log("${number} * ${i} = ${result}");
+// }
+
+// ? Q No. 6 The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here.
+
+/*// ? a. Store a Celsius temperature into a variable.
+
+var celsiustemperature = 25;
+
+// ? b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+
+var Fahrenheittemperature = (celsiustemperature * 9/5) + 32;
+document.write(celsiustemperature + "°C is" + Fahrenheittemperature + "°F" + "<br>");
+
+// ? c. Now store a Fahrenheit temperature into a variable.
+
+var Fahrenheittemperature = 70;
+
+
+// ? d. Convert it to Celsius & output “NNoF is NNoC”.
+
+var celsiustemperature = (Fahrenheittemperature - 32) * 5/9;
+document.write(Fahrenheittemperature + "°F is" + celsiustemperature + "°C" + "<br>");
+*/
+
+// ? Q No. 7 Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+// Compute the total cost & show the receipt in your browser
+/*
+var item1 = 650;
+var quantity1 = 3;
+var item2 = 100;
+var quantity2 = 7;
+
+var total = (item1 * quantity1) + (item2 * quantity2)
+
+document.write("price of item1 is" + item1 + "<br>");
+document.write("quality of item1 is" + quantity1 + "<br>");
+document.write("price of item2 is" + item2 + "<br>");
+document.write("quality of item2 is" + quantity2 + "<br>");
+
+document.write("Total cost of order is" + total + "<br>");
+*/
+
+/*// ? Q No. 8  Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser
+
+var num1 = 900;
+var num2 = 804;
+
+var percentage = (num2/num1) * 100;
+
+document.write("total marks: " + num1 + "<br>");
+document.write("mark obtained: " + num2 + "<br>");
+document.write("percentage: " + percentage + "<br>");
+*/
+
+// ? Q No. 9 Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees. Perform all calculations in a single expression. 
+/*// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee
+
+var PKR1 = 306;
+var $ = 10;
+var PKR2 = 82;
+var Riyal = 25;
+
+var PKRto$ = PKR1 * $;
+var PKRtoRiyal = PKR2 * Riyal;
+var total = PKRto$ + PKRtoRiyal;
+
+document.write("1 PKR convert 10 $ is:" + PKRto$ + "<br>");
+document.write("1 PKR convert 25 Riyal is:" + PKRtoRiyal + "<br>");
+document.write("total currency in PKR is :" + total + "<br>");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
